5fd75b7c5cc5095c0306510398c5fe1900375062
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-07-13 15:45:47 +0200

    + Added random numbers to the bomb array / array for user-selected numbers

--- js/script.js
+++ js/script.js
@@ -7,3 +7,33 @@ Ogni  volta che l'utente sceglie un numero che non è presente tra le bombe,
 guadagna un punto e poi gli chiediamo un altro numero.
 Se il numero scelto dall'utente è presente tra i numeri bomba, la partita termina.
 Quando la partita termina, comunichiamo all'utente il suo punteggio. */
+
+//? #1: Creare un array per contenere le nostre bombe;
+//? #2: Generare numeri random da inserire nell'array di bombe finchè non arrivo a 16 numeri; 
+//? #3: Creare un array che conterrà i numeri scelti dall'utente;
+
+//? #4: Chiedere al giocatore un numero; 
+//? #5: Controllo se è un numero già presente nell'array dei numeri scelti --> se è già presente continuo a chiedere;
+//? #5: Controllo se il numero scelto è all'interno dell'array di bombe --> se è presente la partita termina;
+//? #6: Se il numero non è presente in entrambi gli array lo sggiungo all'array di numeri scelti dall'utente
+
+//? #7: Stampare il messaggio "Game over";
+//? #8: Comunicare all'utente il punteggio raggiunto; 
+//? #9: Ricominciare il gioco. 
+
+// Array bombe
+const bombsNumber = [];
+console.log("Array bombs: ", bombsNumber);
+
+// Numeri random array bombe
+while (bombsNumber.length < 16) {
+    const numberRandom = Math.floor(Math.random() * (101 - 1) + 1);
+    bombsNumber.push(numberRandom)
+}
+console.log(bombsNumber);
+
+// Array numeri utente 
+const userNumber = [];
+
+
+
